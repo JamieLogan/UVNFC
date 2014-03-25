b73ff14c3fa18c2af71a03b89b47697bb6253250
jaimelogan <jamielogan91@googlemail.com>
2014-03-25 15:55:36 +0000

    added androidplot lib, not yet implamented

--- UVNFC/src/main/java/uk/ac/gla/uvnfc/DBResultActivity.java
+++ UVNFC/src/main/java/uk/ac/gla/uvnfc/DBResultActivity.java
@@ -1,14 +1,14 @@
 package uk.ac.gla.uvnfc;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import com.androidplot.series
+
 
 public class DBResultActivity extends ActionBarActivity {
 
